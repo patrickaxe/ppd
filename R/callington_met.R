0ecad29d26c94edeb495c73c0615a79a65c4e4ca
Patrick Di <uqpdi@uq.edu.au>
2021-10-02 16:40:40 +1000

    update

--- R/callington_met.R
+++ R/callington_met.R
@@ -5,7 +5,6 @@
 #' 
 #' @param UserName as "QLDUni" by default
 #' @param PassWord as "Q1dUn!" by default
-#' @param startdate as "17/May/2021" by default, alternatively add "%20HH:MM:SS"
 #' 
 #' @return output
 #' 
@@ -14,7 +13,10 @@
 #' @author patrickaxe
 
 
-callington_met<-function(UserName,PassWord,startdate){
+
+
+
+callington_met<-function(UserName,PassWord){
   
   require(tidyverse)
   require(xml2)
@@ -23,89 +25,86 @@ callington_met<-function(UserName,PassWord,startdate){
   require(XML)
   require(plyr)
   require(data.table)
+  require(lubridate)
   
+  # startdate <- format(Sys.time(),"%Y-%m-%d %T")
   
-  url<-paste0("https://www.outpostcentral.com/api/2.0/dataservice/mydata.aspx","?userName=",UserName, "&password=",PassWord,"&dateFrom=", startdate)
-  # httr::set_config( config( ssl_verifypeer = 0L ))
-  # g<-GET(paste0(url, "?userName=", UserName,  "&password=", PassWord, "&dateFrom=", startdate))
-  xml_df <- read_xml(url, encoding = "UTF-8")
+  mth<-format(Sys.time(),"%m")
   
-  namelist<-xml_text(xml_find_all(xml_df, "//opdata:input/name"))
-  # https://cran.r-project.org/web/packages/xml2/vignettes/modification.html
-  grandchildren<-xml_children(xml_children(xml_children(xml_children(xml_df)[[1]])[[1]])[8])
+  url_list <- as.character()
+  for (month in 5:mth ){
+    link<-paste0("https://www.outpostcentral.com/api/2.0/dataservice/mydata.aspx","?userName=",UserName, "&password=",PassWord,"&dateFrom=",paste0(paste("2021-"),month,paste("-01 00:00:00")))
+    url_list <- c(url_list,link)
+     print(url_list)
+  } 
+  temp2 <- NA
+  rain2 <- NA
+  radn2 <- NA
+  for (link in url_list) {
+    xml_df<-url(link,"rb")
+    xml_df<-read_xml(xml_df)
+    print(xml_df)
+    namelist<-xml_text(xml_find_all(xml_df, "//opdata:input/name"))
+    grandchildren<-xml_children(xml_children(xml_children(xml_children(xml_df)[[1]])[[1]])[8])
+    names(grandchildren) <- namelist
+    
+    temp<-grandchildren[[ namelist[!grepl("#", namelist) & !grepl("Pressure", namelist) & !grepl("Humidity", namelist)& !grepl("Signal", namelist) & !grepl("Delta T", namelist) & !grepl("Counter 2", namelist) & !grepl("Rainfall", namelist) & !grepl("Battery", namelist) & !grepl("ADC1", namelist) & !grepl("Decagon VP4", namelist)]
+    ]]
+    
+    temp<-xml_children(temp)[[6]]
+    temp <- xmlParse(temp) 
+    # rootnode<-xmlRoot(test) 
+    # rootnode[1]
+    temp<-xmlToDataFrame(temp)
+    temp2 <- rbind(temp2,temp)
+    
+    rain<-grandchildren[["Rainfall"]]
+    rain<-xml_children(rain)[[6]]
+    rain <- xmlParse(rain) 
+    rain<-xmlToDataFrame(rain)
+    rain2 <- rbind(rain2,rain)
+    
+    radn<-grandchildren[["#0 Solar Radiation W/m2"]]
+    radn<-xml_children(radn)[[6]]
+    radn <- xmlParse(radn) 
+    radn<-xmlToDataFrame(radn)
+    radn2 <- rbind(radn2,radn)
+  }
   
-  names(grandchildren) <- namelist
+  colnames(temp2) <- c("Temp","Date")
+  temp2$Date <- as.POSIXct(temp2$Date, format = "%Y-%m-%dT%H:%M:%S")
+  temp2$Date <- round(temp2$Date,units="mins")
+  temp2$Temp <- as.numeric(temp2$Temp)
+  colnames(rain2) <- c("Rain","Date")
+  rain2$Date <- as.POSIXct(rain2$Date, format = "%Y-%m-%dT%H:%M:%S")
+  rain2$Date <- round(rain2$Date,units="mins")
+  rain2$Rain <- as.numeric(rain2$Rain)
+  colnames(radn2) <- c("Radn","Date")
+  radn2$Date <- as.POSIXct(radn2$Date, format = "%Y-%m-%dT%H:%M:%S")
+  radn2$Date <- round(radn2$Date,units="mins")
+  radn2$Radn <- as.numeric(radn2$Radn)
   
-  temp<-grandchildren[[ namelist[!grepl("#", namelist) & !grepl("Pressure", namelist) & !grepl("Humidity", namelist)& !grepl("Signal", namelist) & !grepl("Delta T", namelist) & !grepl("Counter 2", namelist) & !grepl("Rainfall", namelist) & !grepl("Battery", namelist) & !grepl("ADC1", namelist) & !grepl("Decagon VP4", namelist)]
-  ]]
+  fullvals <- temp2 %>% full_join(rain2,by="Date") %>% 
+    full_join(radn2,by="Date") %>% 
+    na.omit() %>%
+    mutate(day_til_now = floor(as.double(difftime(now(),Date,units = "day")))) %>%
+    mutate(day = floor(as.double(difftime(Date,"2021-01-01 00:00:00",units = "day")))) 
   
+  maxt<-as.data.frame(aggregate(Temp~day,data=fullvals,FUN=max)) %>% 
+    dplyr::rename("maxt"="Temp")
+  mint<-as.data.frame(aggregate(Temp~day,data=fullvals,FUN=min)) %>%
+    dplyr::rename("mint"="Temp")
+  rain<-as.data.frame(aggregate(Rain~day,data=fullvals,FUN=sum)) %>%
+    dplyr::rename("rain"="Rain")
+  radi<-as.data.frame(aggregate(Radn~day,data=fullvals,FUN=sum)) %>% 
+    mutate(radn = Radn*15*60/1000000)
   
-  temp<-xml_children(temp)[[6]]
-  temp <- xmlParse(temp) 
-  # rootnode<-xmlRoot(test) 
-  # rootnode[1]
-  temp<-xmlToDataFrame(temp)
+ 
+
+  dfsub<-cbind(year="2021", maxt,radi,mint,rain) %>%
+    subset(, select=c(year, day, radn, maxt, mint,rain ))
   
-  
-  rain<-grandchildren[["Rainfall"]]
-  rain<-xml_children(rain)[[6]]
-  rain <- xmlParse(rain) 
-  rain<-xmlToDataFrame(rain)
-  
-  
-  radn<-grandchildren[["#0 Solar Radiation W/m2"]]
-  radn<-xml_children(radn)[[6]]
-  radn <- xmlParse(radn) 
-  radn<-xmlToDataFrame(radn)
-  
-  
-  systime<-as.Date(Sys.time())
-  daydif<-as.numeric(systime-as.Date(paste0(as.year(Sys.time()),"-01-01")))
-  
-  
-  temp<-temp %>% 
-    mutate(Date = as.Date(date)) %>%
-    mutate(day_til_now = dense_rank(desc(Date))) %>%
-    mutate(day = daydif+2-day_til_now)
-  
-  rain<-rain %>% 
-    mutate(Date = as.Date(date)) %>%
-    mutate(day_til_now = dense_rank(desc(Date))) %>%
-    mutate(day = daydif+2-day_til_now)
-  
-  
-  radn<-radn %>% 
-    mutate(Date = as.Date(date)) %>%
-    mutate(day_til_now = dense_rank(desc(Date))) %>%
-    mutate(day = daydif+2-day_til_now)
-  
-  
-  
-  
-  temp<-temp[!(temp$Date== "2021-05-17" | temp$Date==as.character(systime)),]
-  
-  
-  mint<-aggregate(value ~ day, temp, min) %>%
-    dplyr::rename("mint" = "value")
-  maxt<-aggregate(value ~ day, temp, max) %>%
-    dplyr::rename("maxt" = "value")
-  
-  
-  
-  rain<-rain[!(rain$Date== "2021-05-17" | rain$Date==as.character(systime)),]
-  raindata <-aggregate(as.numeric(value)~day, rain, sum) %>%
-    dplyr::rename("rain" = "as.numeric(value)")
-  
-  
-  radn<-radn[!(radn$Date== "2021-05-17" | radn$Date==as.character(systime)),]
-  radn$value<-as.numeric(radn$value)*15*60/1000000
-  radndata<-aggregate(radn$value,by = list(radn$day), FUN = sum) %>%
-    dplyr::rename(c("radn" = "x", "day"="Group.1"))
-  
-  
-  dfsub<-cbind(year="2021",radndata, maxt,mint,raindata) %>%
-    subset(, select=which(!duplicated(names(.)))) 
-  
+
   
   meanT<-(as.numeric(dfsub$maxt)+as.numeric(dfsub$mint))/2
   tav<-mean(meanT)
@@ -152,6 +151,4 @@ callington_met<-function(UserName,PassWord,startdate){
               row.names = FALSE,
               col.names = FALSE,
               append = TRUE, quote=FALSE)
-  
-  
-}
+}  
